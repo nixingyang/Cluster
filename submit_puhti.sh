a856316e0f4e247edd2341ca93cd34b4cfa8656c
nixingyang <nixingyang@users.noreply.github.com>
2020-03-11 15:51:47 +0200

    Update the submit script specifically for Puhti.

--- submit_puhti.sh
+++ submit_puhti.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Set the billing project
+#SBATCH --account=project_2000052
+
 # Allocate CPU resource
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 
 # Allocate memory, time quota and GPU resource
 #SBATCH --mem=60G
-#SBATCH --time=6-23:59:00
+#SBATCH --time=2-23:59:00
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:teslav100:1
-#SBATCH --constraint=gpumem_32
+#SBATCH --gres=gpu:v100:1
 
 # Load ~/.bashrc
 source ~/.bashrc
